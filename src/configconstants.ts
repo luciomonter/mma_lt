72aac1378fd88c4a2e775d53446e169523eff3dc
luciomonter <luciomonter@gmail.com>
2018-12-04 18:12:36 +0100

    again bind to url

--- src/configconstants.ts
+++ src/configconstants.ts
@@ -17,7 +17,7 @@ export class CoreConfigConstants {
     static app_id = 'com.skilla.bccmoodlemobile';
     static appname = 'GBI Accademy';
     static desktopappname = 'GBI Accademy Desktop';
-    static versioncode = 3520;
+    static versioncode = 3521;
     static versionname = '3.5.2';
     static cache_expiration_time = 300000;
     static default_lang = 'it';
@@ -31,9 +31,8 @@ export class CoreConfigConstants {
     static gcmpn = '';
     static customurlscheme = 'skilla_bccmoodlemobile';
     static siteurl = 'http://svilmdlaxapp.amicucci.local/bcc/';
-    static multisitesdisplay = '';
     static skipssoconfirmation = false;
     static forcedefaultlanguage = false;
     static privacypolicy = 'https://moodle.org/mod/page/view.php?id=8148';
-    static compilationtime = 1543942436212;
+    static compilationtime = 1543943522379;
 }
