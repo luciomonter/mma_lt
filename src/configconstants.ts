365050712ef5a550644604ab24522a50e66a41e5
luciomonter <luciomonter@gmail.com>
2018-12-11 18:04:19 +0100

    gulpato

--- src/configconstants.ts
+++ src/configconstants.ts
@@ -34,5 +34,5 @@ export class CoreConfigConstants {
     static skipssoconfirmation = false;
     static forcedefaultlanguage = false;
     static privacypolicy = 'http://www.gruppobancarioiccrea.it/Pagine/Utility/Privacy.aspx';
-    static compilationtime = 1544547668156;
+    static compilationtime = 1544547837707;
 }
