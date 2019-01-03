e8f6f4c7fd0fd95a994491bf35d766f31dc44b1a
luciomonter <luciomonter@gmail.com>
2019-01-03 11:44:07 +0100

    try to fix

--- src/configconstants.ts
+++ src/configconstants.ts
@@ -34,5 +34,5 @@ export class CoreConfigConstants {
     static skipssoconfirmation = false;
     static forcedefaultlanguage = false;
     static privacypolicy = 'http://www.gruppobancarioiccrea.it/Pagine/Utility/Privacy.aspx';
-    static compilationtime = 1546504001728;
+    static compilationtime = 1546512225697;
 }
