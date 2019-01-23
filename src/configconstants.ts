12ccbddc3e59dfdc42bd8409b8b7e55f6d4935e5
luciomonter <luciomonter@gmail.com>
2019-01-23 09:49:00 +0100

    bind to SVIL

--- src/configconstants.ts
+++ src/configconstants.ts
@@ -30,9 +30,9 @@ export class CoreConfigConstants {
     static demo_sites = '';
     static gcmpn = '';
     static customurlscheme = 'axbcc';
-    static siteurl = 'https://gbiacademy.bcc.it/';
+    static siteurl = 'http://svilmdlaxapp.amicucci.local/bcc/';
     static skipssoconfirmation = false;
     static forcedefaultlanguage = false;
     static privacypolicy = 'https://gbiacademy.bcc.it/afmood/privacy.html';
-    static compilationtime = 1547134431318;
+    static compilationtime = 1548233026219;
 }
