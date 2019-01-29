cf9add6badaf49fe2db4d66d17713c6bd41930fa
luciomonter <luciomonter@gmail.com>
2019-01-29 12:36:57 +0100

    increasing version for republishing

--- src/configconstants.ts
+++ src/configconstants.ts
@@ -17,8 +17,8 @@ export class CoreConfigConstants {
     static app_id = 'com.skilla.ltmoodlemobile';
     static appname = 'Skilla LT2019';
     static desktopappname = 'Skilla LT2019';
-    static versioncode = 362045;
-    static versionname = '3.6.245';
+    static versioncode = 362046;
+    static versionname = '3.6.246';
     static cache_expiration_time = 300000;
     static default_lang = 'en';
     static languages: any = {
@@ -74,5 +74,5 @@ export class CoreConfigConstants {
     static skipssoconfirmation = false;
     static forcedefaultlanguage = false;
     static privacypolicy = 'https://www.skilla.com/privacy_policy_cookie.asp?id=1#privacy1';
-    static compilationtime = 1548761227970;
+    static compilationtime = 1548761767965;
 }
--- www/build/17.js
+++ www/build/17.js
@@ -1,6 +1,6 @@
 webpackJsonp([17],{
 
-/***/ 1898:
+/***/ 1897:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -10,7 +10,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(4);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__ngx_translate_core__ = __webpack_require__(1);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__directives_directives_module__ = __webpack_require__(14);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__module_index__ = __webpack_require__(2039);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__module_index__ = __webpack_require__(2038);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__components_components_module__ = __webpack_require__(937);
 // (C) Copyright 2015 Martin Dougiamas
 //
@@ -63,7 +63,7 @@ var CoreSitePluginsModuleIndexPageModule = /** @class */ (function () {
 
 /***/ }),
 
-/***/ 2039:
+/***/ 2038:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
