2b4e894b6fd8689c8bda044e4d8d4960dd83ff66
luciomonter <luciomonter@gmail.com>
2019-02-12 09:51:11 +0100

    ADDING BACK UP for published APP

---
+++ src_bk/configconstants.ts
@@ -0,0 +1,78 @@
+// (C) Copyright 2015 Martin Dougiamas
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// tslint:disable: variable-name
+export class CoreConfigConstants {
+    static app_id = 'com.skilla.ltmoodlemobile';
+    static appname = 'Skilla LT2019';
+    static desktopappname = 'Skilla LT2019';
+    static versioncode = 362047;
+    static versionname = '3.6.247';
+    static cache_expiration_time = 300000;
+    static default_lang = 'en';
+    static languages: any = {
+        'ar': 'عربي',
+        'bg': 'Български',
+        'ca': 'Català',
+        'cs': 'Čeština',
+        'da': 'Dansk',
+        'de': 'Deutsch',
+        'de-du': 'Deutsch - Du',
+        'el': 'Ελληνικά',
+        'en': 'English',
+        'en-us': 'English - United States',
+        'es': 'Español',
+        'es-mx': 'Español - México',
+        'eu': 'Euskara',
+        'fa': 'فارسی',
+        'fi': 'Suomi',
+        'fr': 'Français',
+        'he': 'עברית',
+        'hr': 'Hrvatski',
+        'hu': 'magyar',
+        'id': 'Indonesian',
+        'it': 'Italiano',
+        'ja': '日本語',
+        'km': 'ខ្មែរ',
+        'kn': 'ಕನ್ನಡ',
+        'ko': '한국어',
+        'lt': 'Lietuvių',
+        'mr': 'मराठी',
+        'nl': 'Nederlands',
+        'no': 'Norsk - bokmål',
+        'pl': 'Polski',
+        'pt': 'Português - Portugal',
+        'pt-br': 'Português - Brasil',
+        'ro': 'Română',
+        'ru': 'Русский',
+        'sr-cr': 'Српски',
+        'sr-lt': 'Srpski',
+        'sv': 'Svenska',
+        'tg': 'Тоҷикӣ',
+        'tr': 'Türkçe',
+        'uk': 'Українська',
+        'zh-cn': '简体中文',
+        'zh-tw': '正體中文'
+    };
+    static wsservice = 'moodle_mobile_app';
+    static wsextservice = 'local_mobile';
+    static gcmpn = '117648747151';
+    static customurlscheme = 'ltmma';
+    static siteurl = 'https://lt.skilla.com/';
+    static multisitesdisplay = '';
+    static skipssoconfirmation = false;
+    static forcedefaultlanguage = false;
+    static privacypolicy = 'https://www.skilla.com/privacy_policy_cookie.asp?id=1#privacy1';
+    static compilationtime = 1549463035161;
+}
